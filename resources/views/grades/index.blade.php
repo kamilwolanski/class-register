5dee41989e91a4feae5d97a8d6bdfeff54a26f15
Kamil <kwolanski3@gmail.com>
2024-11-18 16:17:01 +0100

    poprawienie stylowania widoku ocen

--- resources/views/grades/index.blade.php
+++ resources/views/grades/index.blade.php
@@ -8,21 +8,35 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <div class="container">
-                        @foreach ($groupedGrades as $subjectName => $grades)
-                            <div class="subject">
-                                <h3>Przedmiot: {{ $subjectName }}</h3>
-                                <ul>
-                                    @foreach ($grades as $grade)
-                                        <li>
-                                            Ocena: {{ $grade->grade }} - nauczyciel {{$grade->teacher->name}}
-                                            {{$grade->teacher->surname}}
-                                        </li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endforeach
-                    </div>
+                <div class="container">
+    <table class="min-w-full table-auto border-collapse border border-gray-300 text-sm">
+        <thead>
+            <tr class="bg-gray-100">
+                <th class="px-4 py-2 border border-gray-300 text-left">Przedmiot</th>
+                <th class="px-4 py-2 border border-gray-300 text-left">Oceny</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($groupedGrades as $subjectName => $grades)
+                <tr class="{{ $loop->odd ? 'bg-white' : 'bg-gray-50' }}">
+                    <td class="px-4 py-2 border border-gray-300">{{ $subjectName }}</td>
+                    <td class="px-4 py-2 border border-gray-300">
+                        @if($grades->isEmpty())
+                            <span class="text-gray-500 italic">Brak ocen</span>
+                        @else
+                            @foreach ($grades as $grade)
+                                <span class="badge bg-blue-500 text-white py-1 px-3 rounded-full mr-2">
+                                    {{ $grade->grade }} ({{ $grade->teacher->name }} {{ $grade->teacher->surname }})
+                                </span>
+                            @endforeach
+                        @endif
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
                 </div>
             </div>
         </div>
